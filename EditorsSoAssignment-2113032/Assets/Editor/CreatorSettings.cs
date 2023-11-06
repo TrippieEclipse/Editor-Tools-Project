2f991455ae148c6f5b0c265b0f37cce17bcbb44d
TrippieEclipse <na899@student.aru.ac.uk>
2023-11-06 14:35:17 +0000

    Total Over hall into split tabs
    This is a complete Over hall, The system now works in 2 windows. window one is for weapon building and window two is now for all the settings, lists and editing.

---
+++ EditorsSoAssignment-2113032/Assets/Editor/CreatorSettings.cs
@@ -0,0 +1,207 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+public class CreatorSettings : EditorWindow
+{
+    public static event Action<CreatorSettings> ActiveCreatorCall;
+
+    public NewGunCreater ngc;
+    public bool ngcNull = true;
+
+    [MenuItem("My Tools/Main Windows/Creator Settings")]
+    public static void Init()
+    {
+
+        CreatorSettings wnd = GetWindow<CreatorSettings>();
+
+        wnd.minSize = new Vector2(500, 500);
+        wnd.maxSize = new Vector2(1920, 720);
+    }
+
+    private void Update()
+    {
+        if(ngc == null) 
+        {
+            ngcNull = true;
+        }
+        else 
+        {
+            ngcNull = false;
+        }
+    }
+
+    private void OnGUI()
+    {
+        switch (ngcNull) 
+        {
+            case false:
+                
+                ngc.settingsWind = this;
+
+                #region Display Lists
+
+                #region Body List
+                ScriptableObject targetBody = ngc;
+                SerializedObject soBody = new SerializedObject(targetBody);
+                SerializedProperty stringsPropertyBody = soBody.FindProperty("bodyList");
+
+                EditorGUILayout.PropertyField(stringsPropertyBody, true);
+                soBody.ApplyModifiedProperties();
+
+                #region Buttons For Body
+
+                if (GUILayout.Button("Add To Body List"))
+                {
+
+                    foreach (GameObject obj in Selection.gameObjects)
+                    {
+                        if (!ngc.bodyList.Contains(obj) && obj.CompareTag("Body"))
+                        {
+                            ngc.bodyList.Add(obj);
+                        }
+                        else if (ngc.bodyList.Contains(obj))
+                        {
+                            Debug.Log("Asset Already Added");
+                        }
+                        else if (!obj.CompareTag("Body"))
+                        {
+                            Debug.Log("Wrong Asset Type");
+                        }
+
+
+                    }
+                }
+
+
+                if (GUILayout.Button("ClearBodylist"))
+                {
+                    ngc.bodyList.Clear();
+                }
+
+                EditorGUILayout.Space(5);
+                #endregion
+
+                #endregion
+
+                EditorGUILayout.Space(5);
+
+                #region Barrel List
+                ScriptableObject targetBarrel = ngc;
+                SerializedObject soBarrel = new SerializedObject(targetBarrel);
+                SerializedProperty stringsPropertyBarrel = soBarrel.FindProperty("barrelList");
+
+                EditorGUILayout.PropertyField(stringsPropertyBarrel, true);
+                soBarrel.ApplyModifiedProperties();
+
+                #region Buttons For Barrel
+
+                if (GUILayout.Button("Add To Barrel List"))
+                {
+
+                    foreach (GameObject obj in Selection.gameObjects)
+                    {
+                        if (!ngc.barrelList.Contains(obj) && obj.CompareTag("Barrel"))
+                        {
+                            ngc.barrelList.Add(obj);
+                        }
+                        else if (ngc.barrelList.Contains(obj))
+                        {
+                            Debug.Log("Asset Already Added");
+                        }
+                        else if (!obj.CompareTag("Barrel"))
+                        {
+                            Debug.Log("Wrong Asset Type");
+                        }
+
+
+                    }
+                }
+
+
+                if (GUILayout.Button("Clear Barrel list"))
+                {
+                    ngc.barrelList.Clear();
+                }
+
+                EditorGUILayout.Space(5);
+                #endregion
+
+                #endregion
+
+                EditorGUILayout.Space(5);
+
+                #region Handle List
+                ScriptableObject targetHandle = ngc;
+                SerializedObject soHandle = new SerializedObject(targetHandle);
+                SerializedProperty stringsPropertyHandle = soHandle.FindProperty("handleList");
+
+                EditorGUILayout.PropertyField(stringsPropertyHandle, true);
+                soHandle.ApplyModifiedProperties();
+
+                #region Buttons For Handle
+
+                if (GUILayout.Button("Add To Handle List"))
+                {
+
+                    foreach (GameObject obj in Selection.gameObjects)
+                    {
+                        if (!ngc.handleList.Contains(obj) && obj.CompareTag("Handle"))
+                        {
+                            ngc.handleList.Add(obj);
+                        }
+                        else if (ngc.handleList.Contains(obj))
+                        {
+                            Debug.Log("Asset Already Added");
+                        }
+                        else if (!obj.CompareTag("Handle"))
+                        {
+                            Debug.Log("Wrong Asset Type");
+                        }
+
+
+                    }
+                }
+
+
+                if (GUILayout.Button("Clear Handle list"))
+                {
+                    ngc.handleList.Clear();
+                }
+
+                EditorGUILayout.Space(5);
+                #endregion
+
+                #endregion
+
+                EditorGUILayout.Space(5);
+
+                #endregion
+
+                return;
+            case true:
+                EditorGUILayout.HelpBox("To access the creator settings you must open the Gun Creator First. Once the Gun Creator is open, press the button underneath to pair the windows",MessageType.Warning);
+                
+                if (GUILayout.Button("Pair Windows"))
+                {
+                    CalltoCreatorWindow(this);
+                }
+
+                return;
+
+        }
+    }
+
+    public void CalltoCreatorWindow(CreatorSettings csPass) 
+    {
+        ActiveCreatorCall?.Invoke(csPass);
+    }
+
+    public void CreatorWindowReturn(NewGunCreater ngcPass) 
+    {
+        ngc = ngcPass;
+    }
+
+}
--- EditorsSoAssignment-2113032/Assets/Editor/NewGunCreater.cs
+++ EditorsSoAssignment-2113032/Assets/Editor/NewGunCreater.cs
@@ -29,6 +29,8 @@ public enum HandlePartsEnum
 
 public class NewGunCreater : EditorWindow
 {
+    public CreatorSettings settingsWind = null;
+
     public BodyPartsEnum   bodyEnumChoice;
     public BarrelPartsEnum barrelEnumChoice;
     public HandlePartsEnum handleEnumChoice;
@@ -39,16 +41,18 @@ public class NewGunCreater : EditorWindow
     public List<GameObject> handleList = new List<GameObject>();
 
 
-    string weaponName = null;
+    public string weaponName = "";
 
+    
 
-    [MenuItem("My Tools/Room Maker")]
+    [MenuItem("My Tools/Main Windows/Weapon Maker")]
     public static void ShowMyWindow()
     {
         NewGunCreater wnd = GetWindow<NewGunCreater>();
         wnd.titleContent = new GUIContent("WeaponSmith");
+        
 
-        wnd.minSize = new Vector2(1080, 200);
+        wnd.minSize = new Vector2(500, 500);
         wnd.maxSize = new Vector2(1920, 720);
     }
 
@@ -56,26 +60,23 @@ public class NewGunCreater : EditorWindow
 
     private void OnGUI()
     {
-        
-
         #region Weapon Part Selection
 
-        EditorGUILayout.BeginHorizontal();
 
         GUILayout.Label("Weapon Name");
         weaponName = EditorGUILayout.TextField("", weaponName);
 
         EditorGUILayout.Space();
 
-        bodyEnumChoice = (BodyPartsEnum)EditorGUILayout.EnumPopup(new GUIContent("Choose Body Part -->","Select The Body You Would Like For Your Weapon"), bodyEnumChoice);
+        bodyEnumChoice = (BodyPartsEnum)EditorGUILayout.EnumPopup(new GUIContent("Choose Body Part -->","Select The Body You Would Like For Your Weapon"), bodyEnumChoice, GUILayout.MinWidth(250),GUILayout.MaxWidth(300));
 
         EditorGUILayout.Space();
 
-        barrelEnumChoice = (BarrelPartsEnum)EditorGUILayout.EnumPopup(new GUIContent("Choose Barrel Part -->", "Select The Barrel You Would Like For Your Weapon"), barrelEnumChoice);
+        barrelEnumChoice = (BarrelPartsEnum)EditorGUILayout.EnumPopup(new GUIContent("Choose Barrel Part -->", "Select The Barrel You Would Like For Your Weapon"), barrelEnumChoice, GUILayout.MinWidth(250), GUILayout.MaxWidth(300));
 
         EditorGUILayout.Space();
 
-        handleEnumChoice = (HandlePartsEnum)EditorGUILayout.EnumPopup(new GUIContent("Choose Handle Part -->", "Select The Handle You Would Like For Your Weapon"), handleEnumChoice);
+        handleEnumChoice = (HandlePartsEnum)EditorGUILayout.EnumPopup(new GUIContent("Choose Handle Part -->", "Select The Handle You Would Like For Your Weapon"), handleEnumChoice, GUILayout.MinWidth(250), GUILayout.MaxWidth(300));
 
 
         if (GUILayout.Button("Create"))
@@ -85,168 +86,9 @@ public class NewGunCreater : EditorWindow
 
 
 
-        EditorGUILayout.EndHorizontal();
-
         EditorGUILayout.Space(40);
 
         #endregion
-
-        
-
-        #region Adding/Taking Away From List
-
-        #region Body
-
-        if (GUILayout.Button("Add To Body List"))
-        {
-
-            foreach (GameObject obj in Selection.gameObjects)
-            {
-                if (!bodyList.Contains(obj) && obj.CompareTag("Body"))
-                {
-                    bodyList.Add(obj);
-                }
-                else if (bodyList.Contains(obj))
-                {
-                    Debug.Log("Asset Already Added");
-                }
-                else if (!obj.CompareTag("Body"))
-                {
-                    Debug.Log("Wrong Asset Type");
-                }
-
-
-            }
-        }
-
-        if (GUILayout.Button("ListOffBody"))
-        {
-            if (bodyList.Count > 0)
-            {
-                for (int i = 0; i < bodyList.Count; i++)
-                {
-                    Debug.Log(bodyList[i] + " -- index - " + i);
-                }
-            }
-            else
-            {
-                Debug.Log("No Items in list");
-            }
-        }
-
-        if (GUILayout.Button("ClearBodylist"))
-        {
-            bodyList.Clear();
-        }
-
-        EditorGUILayout.Space(5); 
-        #endregion
-
-        #region Barrel
-        if (GUILayout.Button("Add To Barrel List"))
-        {
-
-            foreach (GameObject obj in Selection.gameObjects)
-            {
-                if (!barrelList.Contains(obj) && obj.CompareTag("Barrel"))
-                {
-                    barrelList.Add(obj);
-                }
-                else if (barrelList.Contains(obj))
-                {
-                    Debug.Log("Asset Already Added");
-                }
-                else if (!obj.CompareTag("Barrel"))
-                {
-                    Debug.Log("Wrong Asset Type");
-                }
-            }
-        }
-
-        if (GUILayout.Button("ListOffBarrel"))
-        {
-            if (barrelList.Count > 0)
-            {
-                for (int i = 0; i < barrelList.Count; i++)
-                {
-                    Debug.Log(barrelList[i] + " -- index - " + i);
-
-                }
-            }
-            else
-            {
-                Debug.Log("No Items in list");
-            }
-        }
-
-        if (GUILayout.Button("ClearBarrellist"))
-        {
-            barrelList.Clear();
-        }
-
-        EditorGUILayout.Space(5); 
-        #endregion
-
-        #region Handle
-
-        if (GUILayout.Button("Add To Handle List"))
-        {
-
-            foreach (GameObject obj in Selection.gameObjects)
-            {
-                if (!handleList.Contains(obj) && obj.CompareTag("Handle"))
-                {
-                    handleList.Add(obj);
-                }
-                else if (handleList.Contains(obj))
-                {
-                    Debug.Log("Asset Already Added");
-                }
-                else if (!obj.CompareTag("Handle"))
-                {
-                    Debug.Log("Wrong Asset Type");
-                }
-            }
-        }
-
-        if (GUILayout.Button("ListOffHandlel"))
-        {
-            if (handleList.Count > 0)
-            {
-                for (int i = 0; i < handleList.Count; i++)
-                {
-                    Debug.Log(handleList[i] + " -- index - " + i);
-
-                }
-            }
-            else
-            {
-                Debug.Log("No Items in list");
-            }
-        }
-
-        if (GUILayout.Button("ClearHandlelist"))
-        {
-            handleList.Clear();
-        }
-
-        #endregion
-
-        #endregion
-
-
-        EditorGUILayout.Space(5);
-
-
-        ScriptableObject target = this;
-        SerializedObject so = new SerializedObject(target);
-        SerializedProperty stringsProperty = so.FindProperty("bodyList");
-
-        EditorGUILayout.PropertyField(stringsProperty, true);
-        so.ApplyModifiedProperties();
-
-
-
     }
     public void CreateWeaponSO() 
     {
@@ -275,16 +117,33 @@ public class NewGunCreater : EditorWindow
         }
         
     }
+    
+    
 
     private void OnEnable()
     {
-        
+        CreatorSettings.ActiveCreatorCall += CallToSettings;
+
+        string bodyListData = EditorPrefs.GetString("bodyList", JsonUtility.ToJson(this, false));
+        JsonUtility.FromJsonOverwrite(bodyListData, this);
     }
 
 
     private void OnDisable()
     {
-        
-    }
+        if (settingsWind != null) 
+        {
+            settingsWind.Close();
+        }
 
+        string data = JsonUtility.ToJson(this, false);
+        EditorPrefs.SetString("bodyList", data);
+    }
+    
+    public void CallToSettings(CreatorSettings csMainPass) 
+    {
+        settingsWind = csMainPass;
+        settingsWind.ngc = this;
+    }
+   
 }
