5ea821f88afe97c2db6d97ce9fc3f0180f7e985c
Nate Adam <NA899@student.aru.ac.uk>
2023-12-05 16:19:46 +0000

    few changes to UI look. Some functionality changes for overriding  weapons with the same name

--- EditorsSoAssignment-2113032/Assets/Editor/CreatorSettings.cs
+++ EditorsSoAssignment-2113032/Assets/Editor/CreatorSettings.cs
@@ -15,28 +15,21 @@ public class CreatorSettings : EditorWindow
     [MenuItem("My Tools/Main Windows/Creator Settings")]
     public static void Init()
     {
-        
         CreatorSettings wnd = GetWindow<CreatorSettings>();
-
-        Rect currentPos = wnd.position;
-
-        currentPos.position = lastPos;
-
-        
-        
         wnd.minSize = new Vector2(500, 500);
         wnd.maxSize = new Vector2(1920, 720);
+
+        Rect currentPos = wnd.position;
+        currentPos.position = lastPos;
     }
-
-
 
     private void Update()
     {
-        if(ngc == null) 
+        if (ngc == null)
         {
             ngcNull = true;
         }
-        else 
+        else
         {
             ngcNull = false;
         }
@@ -44,228 +37,184 @@ public class CreatorSettings : EditorWindow
 
     private void OnGUI()
     {
-        switch (ngcNull) 
+        if (!ngcNull)
         {
-            case false:
-                
-                ngc.settingsWind = this;
+            ngc.settingsWind = this;
+            #region Display Lists
+            #region Body List
+            ScriptableObject targetBody = ngc;
+            SerializedObject soBody = new SerializedObject(targetBody);
+            SerializedProperty stringsPropertyBody = soBody.FindProperty("bodyList");
 
-                #region Display Lists
+            EditorGUILayout.PropertyField(stringsPropertyBody, true, GUILayout.MinWidth(300), GUILayout.MaxWidth(700));
+            soBody.ApplyModifiedProperties();
 
-                #region Body List
-                ScriptableObject targetBody = ngc;
-                SerializedObject soBody = new SerializedObject(targetBody);
-                SerializedProperty stringsPropertyBody = soBody.FindProperty("bodyList");
-
-                EditorGUILayout.PropertyField(stringsPropertyBody, true);
-                soBody.ApplyModifiedProperties();
-
-                #region Buttons For Body
-
-                if (GUILayout.Button("Add To Body List"))
+            if (GUILayout.Button("Add To Body List", GUILayout.MinWidth(300), GUILayout.MaxWidth(400)))
+            {
+                foreach (GameObject obj in Selection.gameObjects)
                 {
-
-                    foreach (GameObject obj in Selection.gameObjects)
+                    if (!ngc.bodyList.Contains(obj) && obj.CompareTag("Body"))
                     {
-                        if (!ngc.bodyList.Contains(obj) && obj.CompareTag("Body"))
-                        {
-                            ngc.bodyList.Add(obj);
-                        }
-                        else if (ngc.bodyList.Contains(obj))
-                        {
-                            Debug.Log("Asset Already Added");
-                        }
-                        else if (!obj.CompareTag("Body"))
-                        {
-                            Debug.Log("Wrong Asset Type");
-                        }
-
-
+                        ngc.bodyList.Add(obj);
+                    }
+                    else if (ngc.bodyList.Contains(obj))
+                    {
+                        Debug.Log("Asset Already Added");
+                    }
+                    else if (!obj.CompareTag("Body"))
+                    {
+                        Debug.Log("Wrong Asset Type");
                     }
                 }
+            }
 
+            if (GUILayout.Button("ClearBodylist", GUILayout.MinWidth(300), GUILayout.MaxWidth(400)))
+            {
+                ngc.bodyList.Clear();
+            }
 
-                if (GUILayout.Button("ClearBodylist"))
+            EditorGUILayout.Space(10);
+            #endregion
+
+            #region Barrel List
+            ScriptableObject targetBarrel = ngc;
+            SerializedObject soBarrel = new SerializedObject(targetBarrel);
+            SerializedProperty stringsPropertyBarrel = soBarrel.FindProperty("barrelList");
+
+            EditorGUILayout.PropertyField(stringsPropertyBarrel, true, GUILayout.MinWidth(300), GUILayout.MaxWidth(700));
+            soBarrel.ApplyModifiedProperties();
+
+            #region Buttons For Barrel
+            if (GUILayout.Button("Add To Barrel List", GUILayout.MinWidth(300), GUILayout.MaxWidth(400)))
+            {
+                foreach (GameObject obj in Selection.gameObjects)
                 {
-                    ngc.bodyList.Clear();
-                }
-
-                EditorGUILayout.Space(5);
-                #endregion
-
-                #endregion
-
-                EditorGUILayout.Space(5);
-
-                #region Barrel List
-                ScriptableObject targetBarrel = ngc;
-                SerializedObject soBarrel = new SerializedObject(targetBarrel);
-                SerializedProperty stringsPropertyBarrel = soBarrel.FindProperty("barrelList");
-
-                EditorGUILayout.PropertyField(stringsPropertyBarrel, true);
-                soBarrel.ApplyModifiedProperties();
-
-                #region Buttons For Barrel
-
-
-
-
-
-                if (GUILayout.Button("Add To Barrel List"))
-                {
-
-                    foreach (GameObject obj in Selection.gameObjects)
+                    if (!ngc.barrelList.Contains(obj) && obj.CompareTag("Barrel"))
                     {
-                        if (!ngc.barrelList.Contains(obj) && obj.CompareTag("Barrel"))
-                        {
-                            ngc.barrelList.Add(obj);
-                        }
-                        else if (ngc.barrelList.Contains(obj))
-                        {
-                            Debug.Log("Asset Already Added");
-                        }
-                        else if (!obj.CompareTag("Barrel"))
-                        {
-                            Debug.Log("Wrong Asset Type");
-                        }
-
-
+                        ngc.barrelList.Add(obj);
+                    }
+                    else if (ngc.barrelList.Contains(obj))
+                    {
+                        Debug.Log("Asset Already Added");
+                    }
+                    else if (!obj.CompareTag("Barrel"))
+                    {
+                        Debug.Log("Wrong Asset Type");
                     }
                 }
+            }
+            if (GUILayout.Button("Clear Barrel list", GUILayout.MinWidth(300), GUILayout.MaxWidth(400)))
+            {
+                ngc.barrelList.Clear();
+            }
 
+            EditorGUILayout.Space(5);
+            #endregion
 
-                if (GUILayout.Button("Clear Barrel list"))
+            #endregion
+
+            EditorGUILayout.Space(5);
+
+            #region Handle List
+            ScriptableObject targetHandle = ngc;
+            SerializedObject soHandle = new SerializedObject(targetHandle);
+            SerializedProperty stringsPropertyHandle = soHandle.FindProperty("handleList");
+
+            EditorGUILayout.PropertyField(stringsPropertyHandle, true, GUILayout.MinWidth(300), GUILayout.MaxWidth(700));
+            soHandle.ApplyModifiedProperties();
+
+            #region Buttons For Handle
+
+            if (GUILayout.Button("Add To Handle List", GUILayout.MinWidth(300), GUILayout.MaxWidth(400)))
+            {
+
+                foreach (GameObject obj in Selection.gameObjects)
                 {
-                    ngc.barrelList.Clear();
-                }
-
-                EditorGUILayout.Space(5);
-                #endregion
-
-                #endregion
-
-                EditorGUILayout.Space(5);
-
-                #region Handle List
-                ScriptableObject targetHandle = ngc;
-                SerializedObject soHandle = new SerializedObject(targetHandle);
-                SerializedProperty stringsPropertyHandle = soHandle.FindProperty("handleList");
-
-                EditorGUILayout.PropertyField(stringsPropertyHandle, true);
-                soHandle.ApplyModifiedProperties();
-
-                #region Buttons For Handle
-
-                if (GUILayout.Button("Add To Handle List"))
-                {
-
-                    foreach (GameObject obj in Selection.gameObjects)
+                    if (!ngc.handleList.Contains(obj) && obj.CompareTag("Handle"))
                     {
-                        if (!ngc.handleList.Contains(obj) && obj.CompareTag("Handle"))
-                        {
-                            ngc.handleList.Add(obj);
-                        }
-                        else if (ngc.handleList.Contains(obj))
-                        {
-                            Debug.Log("Asset Already Added");
-                        }
-                        else if (!obj.CompareTag("Handle"))
-                        {
-                            Debug.Log("Wrong Asset Type");
-                        }
+                        ngc.handleList.Add(obj);
+                    }
+                    else if (ngc.handleList.Contains(obj))
+                    {
+                        Debug.Log("Asset Already Added");
+                    }
+                    else if (!obj.CompareTag("Handle"))
+                    {
+                        Debug.Log("Wrong Asset Type");
+                    }
 
 
+                }
+            }
+
+
+            if (GUILayout.Button("Clear Handle list", GUILayout.MinWidth(300), GUILayout.MaxWidth(400)))
+            {
+                ngc.handleList.Clear();
+            }
+
+            EditorGUILayout.Space(5);
+            #endregion
+
+            #endregion
+
+            EditorGUILayout.Space(5);
+
+            #region Scope List
+            ScriptableObject targetScope = ngc;
+            SerializedObject soScope = new SerializedObject(targetScope);
+            SerializedProperty stringsPropertyScope = soScope.FindProperty("scopeList");
+
+            EditorGUILayout.PropertyField(stringsPropertyScope, true, GUILayout.MinWidth(300), GUILayout.MaxWidth(700));
+            soScope.ApplyModifiedProperties();
+
+            #region Buttons For Handle
+            if (GUILayout.Button("Add To Scope List", GUILayout.MinWidth(300), GUILayout.MaxWidth(400)))
+            {
+                foreach (GameObject obj in Selection.gameObjects)
+                {
+                    if (!ngc.scopeList.Contains(obj) && obj.CompareTag("Scope"))
+                    {
+                        ngc.scopeList.Add(obj);
+                    }
+                    else if (ngc.scopeList.Contains(obj))
+                    {
+                        Debug.Log("Asset Already Added");
+                    }
+                    else if (!obj.CompareTag("Scope"))
+                    {
+                        Debug.Log("Wrong Asset Type");
                     }
                 }
+            }
 
+            if (GUILayout.Button("Clear Scope list", GUILayout.MinWidth(300), GUILayout.MaxWidth(400)))
+            {
+                ngc.scopeList.Clear();
+            }
 
-                if (GUILayout.Button("Clear Handle list"))
-                {
-                    ngc.handleList.Clear();
-                }
+            EditorGUILayout.Space(10);
+            #endregion
+            #endregion
+            #endregion
+        }
+        else
+        { 
+            EditorGUILayout.HelpBox("To access the creator settings you must open the Gun Creator First. Once the Gun Creator is open, press the button underneath to pair the windows", MessageType.Warning);
 
-                EditorGUILayout.Space(5);
-                #endregion
-
-                #endregion
-
-                EditorGUILayout.Space(5);
-
-                #region Scope List
-                ScriptableObject targetScope = ngc;
-                SerializedObject soScope = new SerializedObject(targetScope);
-                SerializedProperty stringsPropertyScope = soScope.FindProperty("scopeList");
-
-                EditorGUILayout.PropertyField(stringsPropertyScope, true);
-                soScope.ApplyModifiedProperties();
-
-                #region Buttons For Handle
-
-                if (GUILayout.Button("Add To Scope List"))
-                {
-
-                    foreach (GameObject obj in Selection.gameObjects)
-                    {
-                        if (!ngc.scopeList.Contains(obj) && obj.CompareTag("Scope"))
-                        {
-                            ngc.scopeList.Add(obj);
-                        }
-                        else if (ngc.scopeList.Contains(obj))
-                        {
-                            Debug.Log("Asset Already Added");
-                        }
-                        else if (!obj.CompareTag("Scope"))
-                        {
-                            Debug.Log("Wrong Asset Type");
-                        }
-
-
-                    }
-                }
-
-
-                if (GUILayout.Button("Clear Scope list"))
-                {
-                    ngc.scopeList.Clear();
-                }
-
-                EditorGUILayout.Space(5);
-                #endregion
-
-                #endregion
-
-                EditorGUILayout.Space(5);
-
-
-
-
-                #endregion
-
-                return;
-            case true:
-                EditorGUILayout.HelpBox("To access the creator settings you must open the Gun Creator First. Once the Gun Creator is open, press the button underneath to pair the windows",MessageType.Warning);
-                
-                if (GUILayout.Button("Pair Windows"))
-                {
-                    CalltoCreatorWindow(this);
-                }
-
-                return;
-
+            if (GUILayout.Button("Pair Windows"))
+            {
+                CalltoCreatorWindow(this);
+            }
         }
     }
 
-    
-
-   
-
-    
-    public void CalltoCreatorWindow(CreatorSettings csPass) 
+    public void CalltoCreatorWindow(CreatorSettings csPass)
     {
         ActiveCreatorCall?.Invoke(csPass);
     }
 
-    public void CreatorWindowReturn(NewGunCreater ngcPass) 
+    public void CreatorWindowReturn(NewGunCreater ngcPass)
     {
         ngc = ngcPass;
     }
--- EditorsSoAssignment-2113032/Assets/Editor/NewGunCreater.cs
+++ EditorsSoAssignment-2113032/Assets/Editor/NewGunCreater.cs
@@ -4,26 +4,23 @@ using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
-
+using PopupWindow = UnityEditor.PopupWindow;
 
 public enum BodyPartsEnum
 {
     LongBody,
     ShortBody
 }
-
 public enum BarrelPartsEnum
 {
     LongBarrel,
     ShortBarrel
 }
-
 public enum HandlePartsEnum
 {
     LongHandle,
     ShortHandle
 }
-
 public enum ScopePartsEnum
 {
     CircleScope,
@@ -35,101 +32,72 @@ public enum ScopePartsEnum
 public class NewGunCreater : EditorWindow
 {
     public CreatorSettings settingsWind = null;
-
     public BodyPartsEnum   bodyEnumChoice;
     public BarrelPartsEnum barrelEnumChoice;
     public HandlePartsEnum handleEnumChoice;
     public ScopePartsEnum  scopeEnumChoice;
-
-
     public List<GameObject> bodyList = new List<GameObject>();
     public List<GameObject> barrelList = new List<GameObject>();
     public List<GameObject> handleList = new List<GameObject>();
     public List<GameObject> scopeList = new List<GameObject>();
-
-
+    public List<String> createdWeaponsList = new List<String>();
     public string weaponName = "";
-
     public bool scopeEnabled;
 
     [MenuItem("My Tools/Main Windows/Weapon Maker")]
     public static void ShowMyWindow()
     {
         NewGunCreater wnd = GetWindow<NewGunCreater>();
-
-
-        
-
         wnd.titleContent = new GUIContent("WeaponSmith");
-
         wnd.minSize = new Vector2(500, 500);
         wnd.maxSize = new Vector2(1920, 720);
     }
-
-
-
     private void OnGUI()
     {
         #region Weapon Part Selection
-
-        
-
-        GUILayout.Label("Weapon Name");
-        weaponName = EditorGUILayout.TextField("", weaponName, GUILayout.MinWidth(300), GUILayout.MaxWidth(400));
-
+        weaponName = EditorGUILayout.TextField("Weapon Name -->", weaponName, GUILayout.MinWidth(300), GUILayout.MaxWidth(550));
         EditorGUILayout.Space();
-
-        bodyEnumChoice = (BodyPartsEnum)EditorGUILayout.EnumPopup(new GUIContent("Choose Body Part -->","Select The Body You Would Like For Your Weapon"), bodyEnumChoice, GUILayout.MinWidth(300),GUILayout.MaxWidth(400));
-
+        bodyEnumChoice = (BodyPartsEnum)EditorGUILayout.EnumPopup(new GUIContent("Choose Body Part -->","Select The Body You Would Like For Your Weapon"), bodyEnumChoice, GUILayout.MinWidth(300),GUILayout.MaxWidth(550));
         EditorGUILayout.Space();
-
-        barrelEnumChoice = (BarrelPartsEnum)EditorGUILayout.EnumPopup(new GUIContent("Choose Barrel Part -->", "Select The Barrel You Would Like For Your Weapon"), barrelEnumChoice, GUILayout.MinWidth(300), GUILayout.MaxWidth(400));
-
+        barrelEnumChoice = (BarrelPartsEnum)EditorGUILayout.EnumPopup(new GUIContent("Choose Barrel Part -->", "Select The Barrel You Would Like For Your Weapon"), barrelEnumChoice, GUILayout.MinWidth(300), GUILayout.MaxWidth(550));
         EditorGUILayout.Space();
-
-        handleEnumChoice = (HandlePartsEnum)EditorGUILayout.EnumPopup(new GUIContent("Choose Handle Part -->", "Select The Handle You Would Like For Your Weapon"), handleEnumChoice, GUILayout.MinWidth(300), GUILayout.MaxWidth(400));
-
+        handleEnumChoice = (HandlePartsEnum)EditorGUILayout.EnumPopup(new GUIContent("Choose Handle Part -->", "Select The Handle You Would Like For Your Weapon"), handleEnumChoice, GUILayout.MinWidth(300), GUILayout.MaxWidth(550));
         EditorGUILayout.Space();
+        scopeEnabled = EditorGUILayout.Toggle(new GUIContent("Enable sight on weapon -->", "Select if you want a scope or not"),scopeEnabled, GUILayout.MinWidth(300), GUILayout.MaxWidth(550));
 
-        scopeEnabled = EditorGUILayout.Toggle(new GUIContent("Enable sight on weapon -->", "Select if you want a scope or not"),scopeEnabled, GUILayout.MinWidth(300), GUILayout.MaxWidth(400));
-
-        switch (scopeEnabled) 
+        if (scopeEnabled) 
         {
-            case (true):
-                scopeEnumChoice = (ScopePartsEnum)EditorGUILayout.EnumPopup(new GUIContent("Choose Scope Part -->", "Select The Scope You Would Like For Your Weapon"), scopeEnumChoice, GUILayout.MinWidth(300), GUILayout.MaxWidth(400));
-                if (GUILayout.Button("Create", GUILayout.MinWidth(300), GUILayout.MaxWidth(400)))
-                {
-                    CreateWeaponSO();
-                }
-                return;
-            case (false):
-                if (GUILayout.Button("Create", GUILayout.MinWidth(300), GUILayout.MaxWidth(400)))
-                {
-                    CreateWeaponSO();
-                }
-                return;
+            scopeEnumChoice = (ScopePartsEnum)EditorGUILayout.EnumPopup(new GUIContent("Choose Scope Part -->", "Select The Scope You Would Like For Your Weapon"), scopeEnumChoice, GUILayout.MinWidth(300), GUILayout.MaxWidth(550));
         }
 
-
+        if (GUILayout.Button("Create", GUILayout.MinWidth(300), GUILayout.MaxWidth(550)))
+        {
+            bool hasWeaponAlreadyBeenCreated = CreatedWeaponListCheck();
+            if (!hasWeaponAlreadyBeenCreated)
+            {
+                CreateWeaponSO();
+                createdWeaponsList.Add(weaponName);
+            }
+            else
+            {
+                if (EditorUtility.DisplayDialog("WeaponOverride", "There is already a weapon with the name " + weaponName, "Override", "Cancel")) { CreateWeaponSO(); }
+                else { return; }
+            }
+        }
         #endregion
     }
     public void CreateWeaponSO() 
     {
         if(weaponName != null && barrelList.Count > 0) 
         {
-
             ScriptableWeapon newScriptableObject = ScriptableObject.CreateInstance<ScriptableWeapon>();
-
             newScriptableObject.weaponName = weaponName;
-
             newScriptableObject.body = bodyList[(int)bodyEnumChoice];
             newScriptableObject.barrel = barrelList[(int)barrelEnumChoice];
             newScriptableObject.handle = handleList[(int)handleEnumChoice];
             newScriptableObject.scopeEnabled = scopeEnabled;
             newScriptableObject.scope = scopeList[(int)scopeEnumChoice];
-
             string path = "Assets/WeaponSO/" + weaponName + ".asset";
-
             UnityEditor.AssetDatabase.CreateAsset(newScriptableObject, path);
         }
         else if (weaponName == null)
@@ -140,37 +108,37 @@ public class NewGunCreater : EditorWindow
         { 
                 Debug.Log("No Items in Barrel list");
         }
-        
     }
-
-    
-
-
-
+    bool CreatedWeaponListCheck()
+    {
+        for (int i = 0; i < createdWeaponsList.Count; i++)
+        {
+            if (createdWeaponsList[i] == weaponName) 
+            {
+                return true;
+            }
+        }
+        return false;
+    }
     private void OnEnable()
     {
         CreatorSettings.ActiveCreatorCall += CallToSettings;
-
         string bodyListData = EditorPrefs.GetString("bodyList", JsonUtility.ToJson(this, false));
         JsonUtility.FromJsonOverwrite(bodyListData, this);
     }
-
-
     private void OnDisable()
     {
         if (settingsWind != null) 
         {
             settingsWind.Close();
         }
-
         string data = JsonUtility.ToJson(this, false);
         EditorPrefs.SetString("bodyList", data);
     }
-    
     public void CallToSettings(CreatorSettings csMainPass) 
     {
         settingsWind = csMainPass;
         settingsWind.ngc = this;
     }
-   
 }
+
